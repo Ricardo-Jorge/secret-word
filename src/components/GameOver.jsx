e6ded73943ebcf0426acb2c27faf4139fe49aeef
Ricardo Jorge <ricardolemos18@hotmail.com>
2024-09-25 20:17:54 -0300

    First commit

---
+++ src/components/GameOver.jsx
@@ -0,0 +1,15 @@
+import "./GameOver.css";
+// eslint-disable-next-line react/prop-types
+const GameOver = ({ retry, score }) => {
+  return (
+    <div>
+      <h1>Fim do Jogo</h1>
+      <h2>
+        A sua pontuação foi: <span>{score}</span>
+      </h2>
+      <button onClick={retry}>Resetar Jogo</button>
+    </div>
+  );
+};
+
+export default GameOver;
